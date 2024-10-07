90b6ab52d3e4ed45e518e2ffd8a508a87a573b06
albus0506 <albusnewton29@gmail.com>
2024-10-07 08:10:09 +0100

    added second answerf

--- project.js
+++ project.js
@@ -7,3 +7,21 @@ if(email.includes(slicedEmail)){
     }else{
         console.log("invalid email")
     }
+
+
+ //2.
+let password = "khaliid55foworA"
+let passwordLastLetter = password.charAt(password.length-1).toUpperCase()
+let passwordFirstLetter = password.charAt(0).toLowerCase()
+let passwordNumber = password.includes(0)
+|| password.includes(1) || password.includes(2) 
+|| password.includes(3) || password.includes(4) 
+|| password.includes(5) || password.includes(6) 
+|| password.includes(7) || password.includes(8) || password.includes(9) 
+
+if(passwordFirstLetter && passwordLastLetter && passwordNumber){
+    console.log("strong password")
+    }else{
+        console.log("weak password")
+    }
+
